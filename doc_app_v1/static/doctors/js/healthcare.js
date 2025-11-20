dcb331c0574988df6d9a38a60fd81e90c3ad8261
Jesso <jessojoypeter@gmail.com>
2025-11-20 18:08:42 +0530

    chore: remove ignored files from tracking

---
+++ doc_app_v1/static/doctors/js/healthcare.js
@@ -0,0 +1,7 @@
+function onPortal(role){
+// Replace alerts with actual navigation when wiring up routes
+alert(role.charAt(0).toUpperCase() + role.slice(1) + ' login clicked');
+}
+function onRegister(){
+alert('Register clicked');
+}
